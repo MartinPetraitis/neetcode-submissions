dcaec87c2245432752ad1c98d661bb310fdf57fe
Martin Petraitis <martinaspetraitis04@gmail.com>
2026-07-14 12:41:34 -0500

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const numSet = new Set(nums);
+        let longest = 0;
+        for(let num of numSet) {
+            if(!numSet.has(num - 1)) {
+                let len = 1;
+                while(numSet.has(num + len)) {
+                    len++;
+                }
+                longest = Math.max(longest, len);
+            }
+        }
+        return longest;
+    }
+}
